1a0d7daffa427d6ee2d5dd5d9d89da308cc97aed
Moiz Baig <moizbaig@example.com>
2026-08-31 03:41:12 +0500

    feat: add dedicated Legal column with Privacy Policy and Terms in footer

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-6">
+                <div class="col-lg-2 col-md-4 col-6">
                     <div class="footer-widget">
                         <h5 class="widget-title">Quick Links</h5>
                         <ul class="list-unstyled footer-menu">
@@ -32,12 +32,10 @@
                             <li><a href="about">About Us</a></li>
                             <li><a href="blogs">Blogs</a></li>
                             <li><a href="contact">Contact Us</a></li>
-                            <li><a href="privacy">Privacy Policy</a></li>
-                            <li><a href="terms">Terms of Service</a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-6">
+                <div class="col-lg-2 col-md-4 col-6">
                     <div class="footer-widget">
                         <h5 class="widget-title">Our Services</h5>
                         <ul class="list-unstyled footer-menu">
@@ -47,7 +45,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-6">
+                <div class="col-lg-2 col-md-4 col-6">
                     <div class="footer-widget">
                         <h5 class="widget-title">SaaS Tools</h5>
                         <ul class="list-unstyled footer-menu">
@@ -66,17 +64,26 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-1 col-md-4 col-6">
+                    <div class="footer-widget">
+                        <h5 class="widget-title">Legal</h5>
+                        <ul class="list-unstyled footer-menu">
+                            <li><a href="privacy">Privacy Policy</a></li>
+                            <li><a href="terms">Terms of Service</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6">
                     <div class="footer-widget">
                         <h5 class="widget-title">Contact Info</h5>
                         <ul class="list-unstyled contact-info text-muted">
                             <li class="d-flex align-items-center mb-2">
                                 <i class="fa-solid fa-envelope text-accent-brand me-2 fs-6"></i>
-                                <a href="mailto:contact@automatixes.com" class="text-decoration-none">contact@automatixes.com</a>
+                                <a href="mailto:contact@automatixes.com" class="text-decoration-none text-truncate" style="font-size: 13px;">contact@automatixes.com</a>
                             </li>
                             <li class="d-flex align-items-center mb-2">
                                 <i class="fa-solid fa-phone text-accent-brand me-2 fs-6"></i>
-                                <a href="tel:+923366920141" class="text-decoration-none">+92 336 6920141</a>
+                                <a href="tel:+923366920141" class="text-decoration-none" style="font-size: 13px;">+92 336 6920141</a>
                             </li>
                         </ul>
                     </div>
