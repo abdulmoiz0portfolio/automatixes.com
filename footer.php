3557b8044873a911259e1af5fc02986d4db72b82
Moiz Baig <moizbaig@example.com>
2026-08-31 03:53:31 +0500

    Move Legal (Privacy Policy, Terms of Service) under Quick Links in footer

--- footer.php
+++ footer.php
@@ -32,6 +32,8 @@
                             <li><a href="about">About Us</a></li>
                             <li><a href="blogs">Blogs</a></li>
                             <li><a href="contact">Contact Us</a></li>
+                            <li><a href="privacy">Privacy Policy</a></li>
+                            <li><a href="terms">Terms of Service</a></li>
                         </ul>
                     </div>
                 </div>
@@ -64,16 +66,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-1 col-md-4 col-6">
-                    <div class="footer-widget">
-                        <h5 class="widget-title">Legal</h5>
-                        <ul class="list-unstyled footer-menu">
-                            <li><a href="privacy">Privacy Policy</a></li>
-                            <li><a href="terms">Terms of Service</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <div class="footer-widget">
                         <h5 class="widget-title">Contact Info</h5>
                         <ul class="list-unstyled contact-info text-muted">
