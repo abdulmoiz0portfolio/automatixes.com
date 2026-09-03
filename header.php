b6217617e893b4f62aad97e3b5511e9dc8b3782e
abdulmoiz0portfolio <bobrober2323@gmail.com>
2026-09-03 21:33:41 +0500

    fix(seo): replace wide uncentered favicon with high-res square dark-chassis logo for Google Search & browsers

--- header.php
+++ header.php
@@ -88,7 +88,11 @@ $og_image = "{$protocol}://{$host}/assets/img/services/ai_automations.jpg";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $active_meta['title']; ?></title>
-    <link rel="icon" type="image/png" href="favicon.png?v=2">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <link rel="shortcut icon" href="favicon.ico">
     <link rel="canonical" href="<?php echo $canonical_url; ?>">
     
     <!-- Meta SEO Tags -->
